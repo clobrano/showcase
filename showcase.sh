fe8f5006eb4f9751d8a2e0211a7a39d7864cb53f
Carlo Lobrano <c.lobrano@gmail.com>
2025-02-07 14:15:21 +0100

    Minor LSP guided fixes and remove the tmux custom extension

--- showcase.sh
+++ showcase.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 # -*- coding: UTF-8 -*-
-: ${SC_PROMPT:=""}
-: ${SC_SPEED:=15}
+: "${SC_PROMPT:="$ "}"
+: "${SC_SPEED:=10}"
 
 init() {
     echo -n "$SC_PROMPT"
     sleep 1
 }
+
 slowtype() {
     local text="$1"
     local prompt_at_the_end=${2:-1}
@@ -18,64 +19,30 @@ slowtype() {
 }
 
 slowtype_and_run() {
-    local command=$@
+    local command=$*
     slowtype "${command}" 0
     sleep 0.5
-    eval ${command}
+    eval "${command}"
     sleep 1
     echo -n "$SC_PROMPT"
 }
 
-slowtype_tmux() {
-    # The command to "type"
-    local COMMAND=$1
-    # The target pane (use `tmux list-panes` to find the target pane index)
-    local TARGET_PANE=$2
-    # Delay between each character (in seconds)
-    local DELAY=0.2
-
-    if [ ! -z $TARGET_PANE ]; then
-        target_pane_field="-t $TARGET_PANE"
-    fi
-
-    # Send each character of the command
-    for ((i=0; i<${#COMMAND}; i++)); do
-        tmux send-keys "$target_pane_field" "${COMMAND:$i:1}"
-        sleep $DELAY
-    done
-
-    # Send Enter to execute the command
-    tmux send-keys "$target_pane_field" Enter
-    sleep 1
-}
-
 run() {
     local filepath=$1
-    # do not loop over the file rows, otherwise, for
+    # do not loop over the file rows in the loop, otherwise, for
     # unknown reasons, the first ssh command will break the loop
-    mapfile -t lines < $filepath  # Read all lines into the array 'lines'
+    mapfile -t lines < "$filepath"  # Read all lines into the array 'lines'
 
     for line in "${lines[@]}"; do
-        line=$(envsubst <<< $line)
-        if [[ "$line" == "@@ start @@" ]]; then
-            clear
-            init
-            continue
-        fi
+        line=$(envsubst <<< "$line")
         if [[ "$line" == \$\ * ]]; then
             # lines starting with $ sign are commands to type and execute
-            slowtype_and_run ${line#"$ "}
+            slowtype_and_run "${line#"$ "}"
             continue
         fi
         if [[ "$line" == \!\ * ]]; then
+            # consider lines starting with exclamation mark as command to execute silently
             eval ${line#"! "}
-            continue
-        fi
-        if [[ "$line" == \!t* ]]; then
-            pane=$(echo $line | awk -F'[: ]' '{print $2}')
-            command=${line#* }
-            echo "(tmux send-keys -t $pane \"$command\" Enter)"
-            tmux send-keys -t $pane "$command" Enter
             continue
         fi
         if [[ "$line" == \?* ]]; then
@@ -83,17 +50,12 @@ run() {
             continue
         fi
         # all the rest is text to type only
-        if [[ "$line" == \t* ]]; then
-            pane=$(echo $line | awk -F'[: ]' '{print $2}')
-            command=${line#* }
-            slowtype_tmux "$command" $pane
-            continue
-        fi
         slowtype "$line"
-    done < $filepath
+    done < "$filepath"
     slowtype "" 0
 }
 
 # MAIN
+clear
 init
 run $1
